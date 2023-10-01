3b0c36c7f06a395daa8e851b04147d39b5361526
Acnivator <96819718+Acnivator@users.noreply.github.com>
2023-10-01 17:48:12 +0530

    Add files via upload

---
+++ SunIncrement.js
@@ -0,0 +1,36 @@
+AFRAME.registerComponent('size', {
+  schema: {
+    rad : {type:"number",default:2}
+  },
+
+  init: function () {
+    var totalyears = 0;
+    var sunEl = document.querySelector("#sun");
+    this.sunIncrement(totalyears,sunEl);
+  },
+
+  update: function () {
+
+  },
+
+  sunIncrement: function (totalyears,sunEl) {
+    setInterval(()=> {
+        var years = 0
+      if (totalyears < 6200000000) {
+        years = totalyears + 100000000;
+        totalyears += 100000000
+
+        this.data.rad += 0.1
+        sunEl.setAttribute("radius", this.data.rad)
+      } 
+      else {
+        sunEl.setAttribute("visible",false)
+        console.log("completed2")
+      }
+    },1000)
+  },
+
+  tick: function (time, timeDelta) {
+
+  }
+});
